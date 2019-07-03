031d8b5b83129895b79063491d53ef24b1608185
zhenxingyan <38336539+pingshaluoyan@users.noreply.github.com>
2019-07-03 17:42:47 +0800

    初始项目配置完成

---
+++ market.py
@@ -0,0 +1,15 @@
+#encoding:utf-8
+from flask import Flask,jsonify,request
+from model.member import db,Member
+app=Flask(__name__)
+#配置数据库连接
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/supermarket"
+db.init_app(app)
+@app.route('/')
+def index():
+    return 'Hellow Flask'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80,debug=True)
