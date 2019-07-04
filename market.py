eba0d5e6350471cecf5d9b6f1714c78afcbb9c89
zhenxingyan <38336539+pingshaluoyan@users.noreply.github.com>
2019-07-04 11:18:41 +0800

    三个功能上传

--- market.py
+++ market.py
@@ -1,24 +1,31 @@
-#encoding:utf-8
-from flask import Flask,jsonify,request
-from model.member import db,Member
-app=Flask(__name__)
-#配置数据库连接
+# encoding:utf-8
+from flask import Flask, jsonify, request
+from model.member import db, Member
+
+app = Flask(__name__)
+# 配置数据库连接
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/supermarket"
 db.init_app(app)
+
+
 @app.route('/')
 def index():
     return 'Hellow Flask'
-@app.route('/initdb',methods=['POST'])
+
+
+@app.route('/initdb', methods=['POST'])
 def init_db():
     db.create_all()
-    ret_dic={
-        'return_code':200,
-        'return_msg':'Init db success'
+    ret_dic = {
+        'return_code': 200,
+        'return_msg': 'Init db success'
     }
     return jsonify(ret_dic)
-@app.route('/members',methods=['POST'])
-@app.route('/members/<condition>',methods=['GET','PATCH'])
+
+
+@app.route('/members', methods=['POST'])
+@app.route('/members/<condition>', methods=['GET', 'PATCH'])
 # 根据实付金额更改用户积分
 def surpermark_member(condition=None):
     # 1.处理创建
@@ -30,11 +37,11 @@ def surpermark_member(condition=None):
             "member": mem_info
         }
         return jsonify(ret_dic)
-    if condition==None:
+    if condition == None:
         if request.method == 'PATCH':
             uid = int(condition.split("_")[-1])
             score = int(request.form['score'])
-            ret_dic = Member.update_member_score(uid,score)
+            ret_dic = Member.update_member_score(uid, score)
             ret_dic['return_code'] = 200
             ret_dic['return_msg'] = 'update score success'
             return jsonify(ret_dic)
@@ -42,22 +49,24 @@ def surpermark_member(condition=None):
         # 写http：//127.0.0.1/members下的程序
         pass
     else:
-        if request.method=='GET':
+        if request.method == 'GET':
             pass
-        elif request.method=='请求方法':
+        elif request.method == '请求方法':
             pass
+
 
 # 查找大于给定积分的用户
 @app.route('/filter/score')
 def get_members_byScore():
-    score=request.args['le']
-    ret_dict=Member.get_member_byScore(score)
-    ret_dict['return_code']=200
-    ret_dict['return_msg']="Filter user success"
+    score = request.args['le']
+    ret_dict = Member.get_member_byScore(score)
+    ret_dict['return_code'] = 200
+    ret_dict['return_msg'] = "Filter user success"
     print (ret_dict)
     return jsonify(ret_dict)
 
-@app.route('/member/uid',methods=['DELETD'])
+
+@app.route('/member/uid', methods=['DELETD'])
 def delete_member():
     if request.method == 'DELETE':
         uid = request.form['uid']
@@ -67,5 +76,6 @@ def delete_member():
         print(ret_dic)
         return jsonify(ret_dic)
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
--- model/member.py
+++ model/member.py
@@ -1,15 +1,18 @@
-#encoding:utf-8
+# encoding:utf-8
 from flask_sqlalchemy import SQLAlchemy
 
-db=SQLAlchemy()
+db = SQLAlchemy()
+
+
 class Member(db.Model):
-    uid=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    tel=db.Column(db.String(11),unique=True,nullable=False)
-    discount = db.Column(db.FLOAT,nullable=False,default=1)
-    score = db.Column(db.Integer,nullable=False,default=0)
-    active = db.Column(db.Integer,nullable=False,default=1)
+    uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    tel = db.Column(db.String(11), unique=True, nullable=False)
+    discount = db.Column(db.FLOAT, nullable=False, default=1)
+    score = db.Column(db.Integer, nullable=False, default=0)
+    active = db.Column(db.Integer, nullable=False, default=1)
 
     __tablename__ = 'members'
+
     # 根据手机号码注册新用户--童一鉴
     @classmethod
     def add_member_by_tel(cls, tel):
@@ -20,20 +23,19 @@ class Member(db.Model):
         ret_dic = cls.search_by_tel(tel)['members'][0]
         return ret_dic
 
-
     @classmethod
     def serch_by_tel(cls, tel):
         member_list = []
         if len(tel) == 11:
             member = Member.query.filter(Member.tel.endswith(tel))
             member_info = {'uid': member.uid, 'tel': member.tel, 'discount': member.disc, 'score': member.score,
-                       'active': member.active}
+                           'active': member.active}
             member_list.append(member_info)
         else:
             db_query = Member.query.filter(Member.tel.endwith(tel))
             for member in db_query:
                 member_info = {'uid': member.uid, 'tel': member.tel, 'discount': member.disc, 'score': member.score,
-                           'active': member.active}
+                               'active': member.active}
                 member_list.append(member_info)
                 ret_dic = {
                     'new_member': member_info,
@@ -42,54 +44,51 @@ class Member(db.Model):
                 }
         return ret_dic
 
-
-
     # 获取积分大于指定值的会员列表
     @classmethod
-    def get_member_byScore(cls,score):
-            member_list=[]
-            # 判断传入的le是否为int类型
-            # 若score是字母，特殊字符的时候，返回输入正确的值
-            # 若score是小数，将score加一在判断。
-            try :
-                sc=int(score)
-                if sc<float(score):
-                    sc+=1
-            except :
-                member_list=['请输入正确的数值']
-                ret_dic={
-                    'members':member_list
-                }
-                return ret_dic
-            # 方法一：从数据库中查找所有用户，
-            # 逐个遍历，找到积分大于给定积分的用户，增添进member_list中
-            members=Member.query.all()
-            for mem in members:
-                if mem.score>=sc:
-                    member_info = {"uid": mem.uid,'tel':mem.tel,'discount':mem.discount,'score':mem.score,'active':mem.active}
-                    member_list.append(member_info)
-            if len(member_list)==0:
-                ret_dic= {
+    def get_member_byScore(cls, score):
+        member_list = []
+        # 判断传入的le是否为int类型
+        # 若score是字母，特殊字符的时候，返回输入正确的值
+        # 若score是小数，将score加一在判断。
+        try:
+            sc = int(score)
+            if sc < float(score):
+                sc += 1
+        except:
+            member_list = ['请输入正确的数值']
+            ret_dic = {
+                'members': member_list
+            }
+            return ret_dic
+        # 方法一：从数据库中查找所有用户，
+        # 逐个遍历，找到积分大于给定积分的用户，增添进member_list中
+        members = Member.query.all()
+        for mem in members:
+            if mem.score >= sc:
+                member_info = {"uid": mem.uid, 'tel': mem.tel, 'discount': mem.discount, 'score': mem.score,
+                               'active': mem.active}
+                member_list.append(member_info)
+        if len(member_list) == 0:
+            ret_dic = {
                 "count": 0,
                 "members": member_list
-                }
-            else:
-                ret_dic = {
+            }
+        else:
+            ret_dic = {
                 "count": len(member_list),
                 "members": member_list
-                }
-            return ret_dic
-            # 方法二：从数据库中查找到积分大于给定积分的用户，遍历增添进member_list中
-            # members = Member.query.filter(Member.score >=int(sc))
-            # for mem in members:
-            #     member_list.append(mem)
-            # ret_dic={
-            #  "count":len(member_list),
-            #  "members":member_list
-            # }
-            # return ret_dic
-
-
+            }
+        return ret_dic
+        # 方法二：从数据库中查找到积分大于给定积分的用户，遍历增添进member_list中
+        # members = Member.query.filter(Member.score >=int(sc))
+        # for mem in members:
+        #     member_list.append(mem)
+        # ret_dic={
+        #  "count":len(member_list),
+        #  "members":member_list
+        # }
+        # return ret_dic
 
     @classmethod  # 根据手机号查询会员信息
     def search_by_tel(cls, tel):
@@ -115,13 +114,12 @@ class Member(db.Model):
     @classmethod
     def delete_member(cls, uid):
         mem = Member.query.all()
-        if mem.uid==uid:
+        if mem.uid == uid:
             db.session.delete(mem)
             db.session.commit()
         ret_dic = {"uid": mem.uid, 'tel': mem.tel, 'discount': mem.discount, 'score': mem.score,
                    'active': mem.active}
         return ret_dic
-
 
     # 根据实付金额更改用户积分
     @classmethod
