400a907c3d37d4154414afc5dde2af8ee48aed90
Liuhebo <1041135120@qq.xom>
2019-07-04 09:12:41 +0800

    liuhebo

--- market.py
+++ market.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:330
 db.init_app(app)
 @app.route('/')
 def index():
-    return 'Hellow Flask'
+    return 'Hello Flask'
 @app.route('/initdb',methods=['POST'])
 def init_db():
     db.create_all()
@@ -17,17 +17,26 @@ def init_db():
         'return_msg':'Init db success'
     }
     return jsonify(ret_dic)
-@app.route('/members',methods=['POST'])
-@app.route('/members/<condition>',methods=['GET','PATCH'])
-def surpermark_member(condition=None):
-    if condition==None:
-        # 写http：//127.0.0.1/members下的程序
-        pass
-    else:
-        if request.method=='GET':
-            pass
-        elif request.method=='请求方法':
-            pass
 
+
+@app.route('/members/<condition>',methods=['GET','POST','PUT','PATCH','DELETE'])
+def member_actions(condition=None):
+    if request.method =='GET':
+        if condition.startswith('tel'):
+            tel = condition.split('_')[-1]
+            ret_dic =Member.serch_by_tel(tel)
+            ret_dic['return_code'] = 200
+            ret_dic['return_msg'] = 'Get Member by tel success'
+            return jsonify(ret_dic)
+    # elif request.method =='POST':
+    #     tel = request.form['tel']
+    #     mem_info = Member.add_memebr(tel)
+    #
+    #     ret_dic = {
+    #         'return_code':200,
+    #         'return_msg':'add member success',
+    #         'member':mem_info
+    #     }
+    #     return jsonify(ret_dic)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='127.0.0.1',port=80,debug=True)
